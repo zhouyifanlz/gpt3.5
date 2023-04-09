8c37bbd73dfdd3ed7092bab0de3e017cd136d68f
zhouyifanlz <130283387+zhouyifanlz@users.noreply.github.com>
2023-04-09 18:00:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# API_KEY = "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" 此key无效
+# API_KEY = "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" 
 API_KEY = "sk-此处填API秘钥"
 API_URL = "https://api.openai.com/v1/chat/completions"
 
